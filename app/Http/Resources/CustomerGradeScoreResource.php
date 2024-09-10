4b0f3a80a92b9e7c13074cc6127fb2beb5cddd32
Roxana <ms.roxanarahimi@gmail.com>
2024-09-10 19:52:33 +0330

    ff

--- app/Http/Resources/CustomerGradeScoreResource.php
+++ app/Http/Resources/CustomerGradeScoreResource.php
@@ -15,7 +15,7 @@ class CustomerGradeScoreResource extends JsonResource
      */
     public function toArray($request)
     {
-        $sum = $this->sector?->score || 0 +$this->meterage+$this->product_variety +$this->product_layout +$this->equipment;
+        $sum = ((integer)$this->sector?->score || 0) + (integer)$this->meterage+$this->product_variety + (integer)$this->product_layout + (integer)$this->equipment;
         $grade = '';
         if ($sum <= 30){
            $grade =  'A';
